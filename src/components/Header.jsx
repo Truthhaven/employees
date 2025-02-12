eda278e1ed3f4af86665c3d0a928d4a98bafa70e
Truth Betts-McCullum <truthbetts@gmail.com>
2025-02-11 23:48:46 -0600

    responsive updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
             <h1 className = "headerTitle"> Our People</h1>
             <hr className="horizontal-line" />
             <div className = "filterText"> 
-            <h1> FILTER BY </h1>
-            <h1> CLEAR ALL </h1>
+            <h1 className = "filterBy"> FILTER BY </h1>
+            <h1 className = "clearAll"> Clear All</h1>
             </div>
             </div>
         </>
