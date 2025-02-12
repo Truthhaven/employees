1c237219c466fd1a789458d3869db3cb928b549b
Truth Betts-McCullum <truthbetts@gmail.com>
2025-02-11 23:09:11 -0600

    change text color to black if border is yellow

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -2,27 +2,24 @@ import "./EmployeeCard.css";
 
 const EmployeeCard = ({ employee }) => {
   const borderColor =
-  employee.yearsAtCompany > 5 ? "gold" :
-  employee.yearsAtCompany <= 1 ? "#ac0000" : 
-  "gray";
+    employee.yearsAtCompany > 5 ? "gold" :
+    employee.yearsAtCompany <= 1 ? "#ac0000" : 
+    "gray";
 
-
-    const yearEmployed = employee.yearsAtCompany === 1 ? "year" : "years";
-
-    return (
-        <div className="card" style={{ "--borderColor": borderColor }}>
-            <img src={employee.profilePicture} alt={employee.name} />
-            <div className="info">
-                <h2 className="name">{employee.name}</h2>
-                <p className="role">{employee.role}</p>
-                <div className="groupedInfo">
-                    <p className="department">{employee.department}</p>
-                    <p className="years"> {employee.yearsAtCompany} {yearEmployed} employed</p>
-                    <p className="email">{employee.email}</p>
-                </div>
-            </div>
+  return (
+    <div className="card" style={{ "--borderColor": borderColor }}>
+      <img src={employee.profilePicture} alt={employee.name} />
+      <div className="info">
+        <h2 className="name">{employee.name}</h2>
+        <p className="role">{employee.role}</p>
+        <div className="groupedInfo">
+          <p className="department">{employee.department}</p>
+          <p className="years">{employee.yearsAtCompany} {employee.yearsAtCompany === 1 ? "year" : "years"} employed</p>
+          <p className="email">{employee.email}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeCard;
