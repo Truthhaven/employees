5525dc2bbf143350616abb7126067046ce830715
Truth Betts-McCullum <truthbetts@gmail.com>
2025-02-12 22:32:05 -0600

    css changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = ({ resetFilters }) => {
         <>
             <div className="headerContainer"> 
                 <h1 className="headerTitle"> Our People</h1>
+                <div className = "searchContent"> 
                 <hr className="horizontal-line" />
                 <div className="filterText"> 
                     <h1 className="filterBy"> FILTER BY </h1>
@@ -12,6 +13,7 @@ const Header = ({ resetFilters }) => {
                     <h1 className="clearAll" onClick={resetFilters}>
                         Clear All
                     </h1>
+                </div>
                 </div>
             </div>
         </>
